0aa74312a7a3a4c5cccb4c3072b79decfaf2ecd2
William Koerich <williamkoerich17@gmail.com>
2025-12-06 16:10:24 -0300

    adicionado validação para aparecer o menu de criar evento apenas quando o usuário for do tipo admin

--- src/routes/AppDrawer.jsx
+++ src/routes/AppDrawer.jsx
@@ -1,4 +1,7 @@
+// src/routes/AppDrawer.js
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { useAuth } from '../context/AuthContext';
+
 import { EventCreateScreen } from '../screens/EventCreate/EventCreateScreen';
 import { HomeScreen } from '../screens/Home/HomeScreen';
 import { MyEventsScreen } from '../screens/MyEvents/MyEventsScreen';
@@ -7,6 +10,8 @@ import { ProfileScreen } from '../screens/Profile/ProfileScreen';
 const Drawer = createDrawerNavigator();
 
 export function AppDrawer() {
+  const { user } = useAuth();
+
   return (
     <Drawer.Navigator>
       <Drawer.Screen
@@ -14,22 +19,27 @@ export function AppDrawer() {
         component={HomeScreen}
         options={{ title: 'Eventos' }}
       />
+
+      <Drawer.Screen
+        name="MyEvents"
+        component={MyEventsScreen}
+        options={{ title: 'Meus Eventos' }}
+      />
+
+      {/* Só aparece se for admin */}
+      {user?.type === 'admin' && (
+        <Drawer.Screen
+          name="EventCreate"
+          component={EventCreateScreen}
+          options={{ title: 'Criar Evento' }}
+        />
+      )}
+
       <Drawer.Screen
         name="Profile"
         component={ProfileScreen}
         options={{ title: 'Perfil do Usuário' }}
       />
-      <Drawer.Screen 
-        name="EventCreate"
-        component={EventCreateScreen}
-        options={{ title: 'Criar Evento'}}
-      />
-      <Drawer.Screen 
-        name="MyEvents"
-        component={MyEventsScreen}
-        options={{ title: 'Meus Eventos'}}
-      />
-
     </Drawer.Navigator>
   );
 }
