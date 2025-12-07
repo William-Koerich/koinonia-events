7b944513828b8741e5f5dd7cf750c1418db12795
William Koerich <williamkoerich17@gmail.com>
2025-12-07 09:03:11 -0300

    adicionado logo no menu

--- src/routes/AppDrawer.jsx
+++ src/routes/AppDrawer.jsx
@@ -1,5 +1,15 @@
 // src/routes/AppDrawer.js
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {
+  Box,
+  Image,
+} from '@gluestack-ui/themed';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+} from '@react-navigation/drawer';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 import { useAuth } from '../context/AuthContext';
 
 import { EventCreateScreen } from '../screens/EventCreate/EventCreateScreen';
@@ -7,13 +17,43 @@ import { HomeScreen } from '../screens/Home/HomeScreen';
 import { MyEventsScreen } from '../screens/MyEvents/MyEventsScreen';
 import { ProfileScreen } from '../screens/Profile/ProfileScreen';
 
+import LogoProgenos from '../../assets/images/logo_page2.png';
+
 const Drawer = createDrawerNavigator();
+
+function CustomDrawerContent(props) {
+  return (
+    <SafeAreaView style={{ flex: 1 }} edges={['top', 'bottom']}>
+      <Box flex={1}>
+        <DrawerContentScrollView
+          {...props}
+          contentContainerStyle={{ paddingTop: 0 }}
+        >
+          <DrawerItemList {...props} />
+        </DrawerContentScrollView>
+
+        {/* Rodapé apenas com a logo, ocupando largura toda */}
+        <Box mt="auto">
+          <Image
+            source={LogoProgenos}
+            alt="Logo Koinonia"
+            resizeMode="contain"
+            h={150}        // aumenta aqui se quiser maior (70, 80...)
+            w="100%"      // ocupa toda a largura do menu
+          />
+        </Box>
+      </Box>
+    </SafeAreaView>
+  );
+}
 
 export function AppDrawer() {
   const { user } = useAuth();
 
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+    >
       <Drawer.Screen
         name="Home"
         component={HomeScreen}
@@ -26,8 +66,7 @@ export function AppDrawer() {
         options={{ title: 'Meus Eventos' }}
       />
 
-      {/* Só aparece se for admin */}
-      {user?.type === 'admin' && (
+      {user?.tipo === 'admin' && (
         <Drawer.Screen
           name="EventCreate"
           component={EventCreateScreen}
